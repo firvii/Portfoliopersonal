ca6ecff452c08cefc5f9f20a855ab72949978477
firvii <hydrarossenblute@gmail.com>
2026-05-23 18:37:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: '/',
   publicDir: 'public',
   build: {
     rollupOptions: {
-      input: [
-        'index.html',
-        'formacion.html',
-        'contacto.html',
-        'habilidades.html',
-        'proyectos.html',
-        'quien-soy.html',
-        'aspiraciones.html',
-      ]
+      input: {
+        main:          resolve(__dirname, 'index.html'),
+        formacion:     resolve(__dirname, 'formacion.html'),
+        contacto:      resolve(__dirname, 'contacto.html'),
+        habilidades:   resolve(__dirname, 'habilidades.html'),
+        proyectos:     resolve(__dirname, 'proyectos.html'),
+        quienSoy:      resolve(__dirname, 'quien-soy.html'),
+        aspiraciones:  resolve(__dirname, 'aspiraciones.html'),
+      }
     }
   }
 })
